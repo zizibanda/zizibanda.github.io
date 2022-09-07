b9b063fe043177a36bd2037bb404bf22d11541e6
Zizi Banda <zizi.banda@plumguide.com>
2022-09-07 17:01:46 +0100

    amendments based on comments from Alex

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-const profile: string = `I am a software developer with a strong passion for Quality Assurance and aspiration to grow my career as a software developer. 
+const profile = `I am a software developer with a strong passion for Quality Assurance and aspiration to grow my career as a software developer. 
 I specialise in establishing and building manual and automated test suites using various different testing frameworks. 
 I take lots of pride in advocating strong testing practices whilst also influencing others to assure quality. 
 I am determined to solve and/or detect problems in an innovative manner, with front-end development, software testing and music as my interests.`
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <header className="App-header" id="home">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1 style={{color: "#E6CB20"}}>ZIZI BANDA</h1>
-        <h3 style={{color: "#E6CB20"}}>QA ENGINEER</h3> 
+        <h1 className="titleFormat">ZIZI BANDA</h1>
+        <h3 className="titleFormat">QA ENGINEER</h3> 
         {/* Add Socials Here */}
         <p>
           {profile}
